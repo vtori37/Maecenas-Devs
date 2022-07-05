343771a9ebf3e18296af5b1835fc741cb1af9911
JonathanKim424 <vizard424@yahoo.com>
2022-07-05 15:59:26 -0600

    created server folder structure and init server capability

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}!`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        });
+    });
+}
+
+startApolloServer(typeDefs, resolvers);
