5f33c5991364a7cf7c05f05c98683b31237298e3
andrewyk99 <andrewyk99@hotmail.com>
2022-07-07 22:09:52 -0600

    changed seeds

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,32 +1,59 @@
-const creatorSeeds = require('./creatorSeed.json');
-const blogPostSeeds = require('./blogPostSeed.json');
 const db = require('../config/connection');
 const { Creator, BlogPost } = require('../models');
 
 db.once('open', async () => {
-    try {
-        await BlogPost.deleteMany({});
-        await Creator.deleteMany({});
+    await BlogPost.deleteMany();
 
-        await Creator.create(creatorSeeds);
-
-        for (let i = 0; i < blogPostSeeds.length; i++) {
-            const { _id, blogAuthor } = await BlogPost.create(blogPostSeeds[i]);
-            const creator = await Creator.findOneAndUpdate(
-                { name: blogAuthor },
-                {
-                    $addToSet: {
-                        blogPosts: _id,
-                    },
-                }
-            );
+    const blogPosts = await BlogPost.insertMany([
+        { 
+            creatorName: "Beginner's Guide to HTML",
+            blogText: "Lorem ipsum blah blah blah"
+        },
+        {
+            creatorName: "Beginner's Guide to CSS!",
+            blogText: "CSS is how to style your page!"
         }
-    }
-    catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
+    ]);
 
-    console.log('Successfully seeded!');
-    process.exit(0);
+    console.log('BlogPosts seeded');
+
+    await Creator.deleteMany();
+
+    const creators = await Creator.insertMany([
+        {
+            creatorName: "Creator 1",
+            about: "Hi! My name is Creator 1, I just recently graduated from UT Austin Bootcamp and I would love to share my experiences with anyone taking the course!",
+            specialties: "HTML, CSS, JavaScript",
+            url: "http://creator1.com",
+            tier1: [],
+            tier2: [],
+            tier3: [],
+            blogPost: blogPosts[0]
+        },
+        {
+            creatorName: "Creator 2",
+            about: "Hi! My name is Creator 2, I just recently graduated from UT Austin Bootcamp and I would love to share my experiences with anyone taking the course!",
+            specialties: "Node.js, Tailwind, React",
+            url: "http://creator2.com",
+            tier1: [],
+            tier2: [],
+            tier3: [],
+            blogPost: blogPosts[1]
+        },
+        {
+            creatorName: "Creator 3",
+            about: "Hi! My name is Creator 3, I just recently graduated from UT Austin Bootcamp and I would love to share my experiences with anyone taking the course!",
+            specialties: "MongoDB, Mongoose, noSQL",
+            url: "http://creator3.com",
+            tier1: [],
+            tier2: [],
+            tier3: [],
+            blogPost: blogPosts[2]
+        }
+
+    ]);
+
+    console.log('Creators seeded');
+
+    process.exit();
 });
