c05db1c1ba565147bdc96cc82f6c63533dad12af
Tori Acer <vrice360@gmail.com>
2022-07-08 19:38:01 -0500

    Blog page design

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../index.css'
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
+    <footer className="footer w-100 mt-auto bg-secondary p-4">
       <div className="container">&copy;2020 by Maecenas Devs</div>
     </footer>
   );
--- client/src/pages/BlogPage.js
+++ client/src/pages/BlogPage.js
@@ -1,17 +1,55 @@
+
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
-export default function BlogPage({ spots, banana }) {
-  // const banana = props.banana; (pass in props)
+import '../index.css'
 
-  // destructure:
-  // const { banana } = props
-  // just enter in props in function and use {banana}
+
+function BlogPage() {
   
-// {banana} passed into BlogPage will allow you to enter {banana} anywhere
-// in the JSX
-  return <div>Hello World {banana}{spots} </div>
+  
+    return (
+      <section className='blog-section'>
+        <div className='containr'>
+          <div className='contains-box'></div>
+          <div className='contains-tabs'>
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3 blog-tab">
+              <Tab eventKey="Blog-Post" title="Blog-Post">
+                <h1> Hi </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia assumenda dolore reprehenderit ullam blanditiis ex sapiente pariatur tempore incidunt facilis?</p>
+              </Tab>
+              <Tab eventKey="Comments" title="Comments">
+              <h4> Hi </h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia assumenda dolore reprehenderit ullam blanditiis ex sapiente pariatur tempore incidunt facilis?</p>
+              <h4> Yuh </h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia assumenda dolore reprehenderit ullam blanditiis ex sapiente pariatur tempore incidunt facilis?</p>
+                <InputGroup className="mb-3">
+                  <Form.Control
+                    placeholder="Recipient's username"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                  />
+                  <Button variant="outline-secondary" id="button-addon2">
+                    Button
+                  </Button>
+                </InputGroup>
+              </Tab>
+              {/* <Tab eventKey="Author" title="Author" disabled>
+          
+              </Tab> */}
+            </Tabs>
+          </div>
+        </div>
+      </section>
+    )
 };
 
+export default BlogPage;
 
 // import { useParams } from 'react-router-dom';
 // import { useQuery } from '@apollo/client';
