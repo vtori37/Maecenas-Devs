5413b029084aa80a8555f377bb132dfe20322b61
JonathanKim424 <vizard424@yahoo.com>
2022-07-17 16:04:06 -0600

    modified PWA files

--- client/public/serviceWorker.js
+++ client/public/serviceWorker.js
@@ -1,15 +1,17 @@
 const CACHE_NAME = 'version-1';
-const urlsToCache = [ 'index.html' , 'offline.html' ];
+const urlsToCache = [
+    './index.html',
+    './manifest.json',
+    './serviceWorker.js'
+];
 
 const self = this;
 
 // Install service worker
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
+        caches.open(CACHE_NAME).then(function (cache) {
                 console.log('Open cache');
-
                 return cache.addAll(urlsToCache);
             })
     )
@@ -18,11 +20,17 @@ self.addEventListener('install', (event) => {
 // Listen for requests
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(() => {
+        caches.match(event.request).then((request) => {
+            if (request) {
+                console.log('responding with cache: ' + event.request.url)
+                return request
+            } else if ( event.request.mode === 'navigate') {
+                return caches.match('./index.html');
+            } else {
+                console.log('File is not cached, fetching: ' + event.request.url)
                 return fetch(event.request)
-                    .catch(() => caches.match('offline.html'))
-            })
+            }
+        })
     )
 });
 
